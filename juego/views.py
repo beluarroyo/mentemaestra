71a5df85018d29a8b6ad6863263a9d3c399c25dc
Belu Arroyo <maribel.gerardway@gmail.com>
2016-08-12 19:48:33 -0300

    modificacion de la vista

--- juego/views.py
+++ juego/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
+
+
+def home (request): 
+    return render(request, 'home.html', 
+        {'titulo': 'Soy un titulo desde la vista',
+          'encuestas': Question.objects.all()})
+
+
 # Create your views here.
