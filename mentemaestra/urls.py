463bb82a63bc50b673ab3b9dd5d84c959f7edc8a
Belu Arroyo <maribel.gerardway@gmail.com>
2016-08-12 19:18:59 -0300

    button en urls

--- mentemaestra/urls.py
+++ mentemaestra/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
+from views import *
 
 urlpatterns = [
+    url(r'^$', home, name='home'),
+    
+    url(r'^vista2/', vista2),
+
+    url(r'^vista3/', vista2),
+    
     url(r'^admin/', admin.site.urls),
+
+
+
 ]
